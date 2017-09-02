6896b51a3b39af662e9282e7f2104a8d1f3b50b1
Khan Mohammad Irteza <irteza.khan@gmail.com>
2017-09-02 01:54:07 -0400

    Initial Commit

---
+++ ConvertAll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConvertAll
+//
+//  Created by Mohammad Irteza Khan on 9/2/17.
+//  Copyright © 2017 Mohammad Irteza Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
